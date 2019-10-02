0d7e9892783e44cac84b51e34d042f6e3d831b22
Hernán Guillermo Dulcey Morán <guille.dulcey@gmail.com>
2019-10-02 13:13:34 -0500

    Fixed some spelling
    Word sequence

--- FibonacciProject/FibonacciLoop.java
+++ FibonacciProject/FibonacciLoop.java
@@ -9,13 +9,13 @@ package FibonacciProject;
 public class FibonacciLoop {
 	public static void main(String[] args) {
 		int value = Integer.parseInt(args[0]);
-		System.out.println("The "+value+" value of the Fibonacci secuence: "+fibonacci(value));
+		System.out.println("The "+value+" value of the Fibonacci sequence: "+fibonacci(value));
 	}
 	
 	public static long fibonacci(int x){
 		long[] n1n2 = new long[2];
 		/*****************************************************************
-		Again the number 0 is considered the first element in the secuence
+		Again the number 0 is considered the first element in the sequence
 		*****************************************************************/
 		n1n2[0] = 0;
 		n1n2[1] = 1;
@@ -26,7 +26,7 @@ public class FibonacciLoop {
 		/******************************************************************************
 		In this case, the base values are stored in an array of two elements.
 		This elements are going to store the values we need to calculate the next value
-		This way it doesn't need to use recursion to solve the secuence.
+		This way it doesn't need to use recursion to solve the sequence.
 		******************************************************************************/
 		for(int i = 2; i < x; i++){
 			long newValue = n1n2[1] + n1n2[0];
